64ac35cdb5f8cf2e9fa7a80d6ae8561db4844550
kunmingren <834719922@qq.com>
2017-02-21 17:09:37 +0800

    恢复版本

--- src/modules/app.js
+++ src/modules/app.js
@@ -58,7 +58,8 @@
 
 
     .constant('CONFIG', {
-        serverUrl: 'http://openvod.cleartv.cn/backend_clearmgt/v1/',
+        // serverUrl: 'http://openvod.cleartv.cn/backend_clearmgt/v1/',
+        serverUrl: 'http://192.168.30.100/backend_clearmgt/v1/',
         uploadImgUrl: 'http://mres.cleartv.cn/upload',
         uploadVideoUrl: 'http://movies.clearidc.com/upload',
         testUrl: 'test/',
